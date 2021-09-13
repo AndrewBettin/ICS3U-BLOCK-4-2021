c28d216c136773ab82e6b7d86cff030d23fe94c2
Andrew Bettin <abettin@bayviewglen.ca>
2021-09-13 14:39:17 -0400

    September 13th 2021 ex. 6,7,8

---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleEight.java
@@ -0,0 +1,31 @@
+package week2;
+
+public class ExampleEight {
+
+    /**
+     * Type Casting
+     */
+    
+    public static void main(String[] args){
+
+        final int NUM_MARKS = 5;
+
+        int markOne = 78, markTwo = 87, markThree = 64, markFour = 95, markFive = 84;
+
+        double average = (double) (markOne + markTwo + markThree + markFour + markFive) / NUM_MARKS;
+
+        int x = (int) y;
+        double y = 3.7;
+
+        double z = (double) 7; // explicit cast
+
+        //int -> double // widening conversion
+        // double -> int // narrowing conversion
+
+        // boolean p = !true;
+
+
+        System.out.println("The average is: " + average);
+    }
+
+}
---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,25 @@
+package week2;
+
+public class ExampleSeven{
+
+    /**
+     * final modifier  - contant
+     */
+
+    public static void main(String[] args){
+        final int x = 7;
+        // x = 6
+
+        final int y;
+        y = 8;
+        /// y++; // cannot midify the variable once it is set if it is constant
+
+        final int NUM_STUDENTS = 18; // naming convention for constants (final variable) is UPPER_CASE with _ for
+        // spaces
+
+        
+
+
+    }  
+}
+
