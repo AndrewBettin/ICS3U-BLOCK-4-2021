8b243f68e19c72fb5759b1b1496f6e1135fccaa1
Andrew Bettin <abettin@bayviewglen.ca>
2021-09-21 14:59:30 -0400

    Week 3 number examples

---
+++ Unit Two - Using objects/week3/MathClassExamples.java
@@ -0,0 +1,19 @@
+package week2;
+
+public class MathClassExamples {
+    public static void main(String[] args){
+        System.out.println(Math.abs(-4));
+        System.out.println(Math.abs(-4.1));
+
+        System.out.println(Math.pow(2,3)) //2 to th power of 3
+
+        int x1 = 5;
+        int y1 = 3;
+
+        int x2 = 10;
+        int y2 = 2;
+
+        double length = Math.sqrt(Math.pow(x2))
+    }
+    
+}
---
+++ Unit Two - Using objects/week3/RandomNumberExamples.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class RandomNumberExamples {
+    public static void main(String[] args){
+        double r = Math.random();
+        System.out.println(r);
+    }
+    
+}
